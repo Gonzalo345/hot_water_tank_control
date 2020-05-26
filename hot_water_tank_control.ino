69e5dd3c11a0c7ed5b5bf7fa6950ef6001e5d17e
Gonzalo - <gonzaloyarvi@hotmail.com>
2020-05-25 22:06:06 -0300

    Add DS18B20 sensor and DS3231 clock

--- hot_water_tank_control.ino
+++ hot_water_tank_control.ino
@@ -7,38 +7,52 @@
 // by Gonzalo Yarv
 // 7:46, 20, 5, 2017
 
+#include "U8glib.h"
+#include <OneWire.h>
+#include <Wire.h>
+#include "RTClib.h"
+#include <DallasTemperature.h>
+#include <DHT.h>
 
-   #include "U8glib.h"
-   #include <OneWire.h>
-   #include <DallasTemperature.h>
-   #include <DHT.h>
+RTC_DS3231 rtc;             // SDA D4 digital 04 pin 7 SCL D5 digital pin 05 pin 08
+char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+const int ledPIN = 13;
+const int boton1 = 3;
+//const int valvePIN = 3;
 
-   char temp_string[2]; //empty char array to hold the converted temp float
-   char hum_string[5];  //empty char array to hold the converted humidity float
-   float hum_float;
+//DS3231  rtc(SDA, SCL);
+char temp_string[3]; //empty char array to hold the converted temp float
+char time_string[6]; //
+char hum_string[5];  //empty char array to hold the converted humidity float
+float hum_float;
 
 // display constructor for u8g library - this one depends on your type of display
-   U8GLIB_ST7920_128X64 u8g(13, 11, 12, U8G_PIN_NONE);
+U8GLIB_ST7920_128X64 u8g(13, 11, 12, U8G_PIN_NONE);
 
 // DS18B20 section ================================================================
 
 // one wire data probe to pin 6
-   #define ONE_WIRE_BUS_PIN 6
+#define ONE_WIRE_BUS_PIN 6
 
 // Setup a oneWire instance to communicate with any OneWire devices
-   OneWire oneWire(ONE_WIRE_BUS_PIN);
-   DallasTemperature sensors(&oneWire);
-   DeviceAddress Probe_01 = {0x28, 0xFF, 0x0A, 0x75, 0xA3, 0x15, 0x04, 0xBC};
+OneWire oneWire(ONE_WIRE_BUS_PIN);
+DallasTemperature sensors(&oneWire);
+DeviceAddress Probe_01 = {0x28, 0xE4, 0xA4, 0x79, 0xA2, 0x00, 0x03, 0xA7};
+
 // each DS18B20 has each own unique address - use address finder sketch
 
 
 // DHT11 section ==================================================================
 // tell the DHT library where to expect a DHT sensor and which type
-   DHT DHT_sens(8, DHT11);                //  connected to pin 8 of the Arduino
+DHT DHT_sens(8, DHT11);                //  connected to pin 8 of the Arduino
 
 
-   void setup() {
+void setup() {
 
+   //pinMode(ledPIN, OUTPUT);
+   //pinMode(resistorPIN, OUTPUT);
+   //pinMode(valvePIN, OUTPUT);
+   pinMode(boton1, INPUT);
    DHT_sens.begin();                       // start DHT sensoring
    sensors.begin();                        // START dallas temperature sensoring
    sensors.setResolution(Probe_01, 10);    // set DS18B20 sensor resolution to 10 bit
@@ -50,40 +64,68 @@
    Serial.print("Number of Devices found on bus = ");
    Serial.println(sensors.getDeviceCount());
    Serial.println ("============================================");
+   
    Serial.print("Getting temperature, humidity... ");
    Serial.println();
+   if (! rtc.begin()) {
+     Serial.println("Couldn't find RTC");
+     while (1);
+   }
+   //rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+   if (rtc.lostPower()) {
+     Serial.println("RTC lost power, lets set the time!");
+     // Comment out below lines once you set the date & time.
+     // Following line sets the RTC to the date & time this sketch was compiled
+     //rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+
+     // Following line sets the RTC with an explicit date & time
+     // for example to set January 27 2017 at 12:56 you would call:
+     // rtc.adjust(DateTime(2017, 1, 27, 12, 56, 0));
+   }
 
 
-  // Select font =================================================================
+   // Select font =================================================================
 
-  //u8g.setFont(u8g_font_unifont);
-  u8g.setFont(u8g_font_5x7);
-  // u8g.setFont(u8g_font_unifont_0_8);
-  // u8g.setFont(u8g_font_courB10);
-  // u8g.setFont(u8g_font_courB08);
-  // u8g.setFont(u8g_font_7x14B);
-  // u8g.setFont(u8g_font_8x13);
-  // u8g.setFont(u8g_font_osb21);
-
-  u8g.setColorIndex(1);                   // Instructs the display to draw with a pixel on.
+   u8g.setFont(u8g_font_5x7);
+   u8g.setColorIndex(1);                   // Instructs the display to draw with a pixel on.
 }
 
 void loop() {
   unsigned char i = 0;
- for (i=0; i<36; i+=8){
+  for (i=0; i<36; i+=8){
     u8g.firstPage();
     do {
       fill(i);
       draw();
-    } while( u8g.nextPage() );
-    delay(1000);
- }
- 
+    }
+    while( u8g.nextPage() );
+    //digitalWrite(ledPIN, HIGH);
+    DateTime now = rtc.now();
+    //digitalWrite(resistorPIN, HIGH);
+    Serial.println("Current Date & Time: ");
+    Serial.print(now.year(), DEC);
+    Serial.print('/');
+    Serial.print(now.month(), DEC);
+    Serial.print('/');
+    Serial.print(now.day(), DEC);
+    Serial.print(" (");
+    Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
+    Serial.print(") ");
+    Serial.print(now.hour(), DEC);
+    Serial.print(':');
+    Serial.print(now.minute(), DEC);
+    Serial.print(':');
+    Serial.print(now.second(), DEC);
+    Serial.println();
+    delay(2000);
+    //digitalWrite(ledPIN, LOW);
+    //digitalWrite(resistorPIN, LOW);
+    delay(2000);
+  }
 }
 
 void draw(){
- //int i;
-  //u8g.setFont(u8g_font_micro);
+
   u8g.setFont(u8g_font_5x7);
   // upper frame ===============================================================
   u8g.drawFrame(65,5,8,8);
@@ -125,10 +167,17 @@ void draw(){
   u8g.drawStr(30,57, "TCte");
   // Time ====================================================================
   u8g.setFont(u8g_font_10x20);
-  u8g.drawStr(5,17, "20:05");
+  DateTime now = rtc.now();
+  dtostrf(now.hour(), 2, 0, time_string);
+  u8g.drawStr(5,17, time_string);
+  u8g.drawStr(22,17, ":");
+  dtostrf(now.minute(), 2, 0, time_string);
+  u8g.drawStr(30,17, time_string);
+  
+  //
   // Temperature =============================================================
-//  u8g.setFont(u8g_font_10x20);
-//  u8g.drawStr(30,42, "24");
+  //  u8g.setFont(u8g_font_10x20);
+  //  u8g.drawStr(30,42, "24");
   // °C ======================================================================
   //u8g.drawCircle(52, 29, 1);
   //u8g.drawStr(53,42, "C");
@@ -137,8 +186,8 @@ void draw(){
   sensors.requestTemperatures();
 
   Serial.print("temperatura: ");
-  //float tempC = sensors.getTempC(Probe_01);
-  float tempC = DHT_sens.readTemperature();
+  float tempC = sensors.getTempC(Probe_01);
+  //float tempC = DHT_sens.readTemperature();
   //float tempC = 17.6;
   Serial.print (tempC,1);
   Serial.println(" *C\t");
@@ -173,7 +222,7 @@ void draw(){
   //u8g.drawStr(40,60, hum_string);
   //u8g.drawStr(75,60, "%");
 
-}
+  }
 }
 
 void fill(unsigned char percent) {     // Filled test
